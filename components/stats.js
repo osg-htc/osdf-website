73bb9d76559fe8a79a0b91670da34b5608b466fa
Derek Weitzel <djw8605@gmail.com>
2022-03-10 08:55:08 -0600

    Pretty-ifying the stats cards more

--- components/stats.js
+++ components/stats.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import useSWR from 'swr'
-import { ArrowUpIcon, FolderDownloadIcon } from '@heroicons/react/solid';
+import { ArrowUpIcon } from '@heroicons/react/solid';
+import { CloudDownloadIcon, FolderDownloadIcon } from '@heroicons/react/outline';
 
 function fetcher(url) {
   return fetch(url).then(r => r.json())
@@ -69,8 +70,8 @@ export default function Stats() {
     <>
       <section className='bg-gray-900 md:pr-6 pt-6'>
         <div className='container grid md:grid-cols-2 gap-4'>
-          <StatCard title='Bytes Read' value={read} rate={bytesPerSecond} humanFunction={humanFileSize} subtext={"Last 30 days"} />
-          <StatCard title='Files Read' value={filesRead} rate={filesPerSecond} humanFunction={numberWithCommas} subtext={"Last 30 days"} icon={<FolderDownloadIcon />} />
+          <StatCard title='Bytes Read' value={read} rate={bytesPerSecond} humanFunction={humanFileSize} subtext={"Last 30 days"} icon={<CloudDownloadIcon className='h-10 w10' />} />
+          <StatCard title='Files Read' value={filesRead} rate={filesPerSecond} humanFunction={numberWithCommas} subtext={"Last 30 days"} icon={<FolderDownloadIcon className="h-10 w-10" />} />
         </div>
       </section>
     </>
@@ -113,43 +114,33 @@ function StatCard({ title, value, unit, rate, humanFunction, subtext, icon }) {
   }, []);
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-full h-full p-4 text-white bg-gray-800 rounded shadow-2xl">
-        <div className='stats-icon'>
-          {icon}
+      <div class="p-4 bg-gray-800 rounded shadow-2xl flex flex-col text-white transition ease-in-out delay-75 hover:scale-105 hover:bg-gray-700">
+        <div class="flex flex-row w-full">
+          <div class="flex flex-col justify-center">
+            <h4 class="text-lg text-gray-300 uppercase">{title}</h4>
+            <h3 className='text-2xl font-bold'>{localValue ? (
+              humanFunction(value + localAdd)
+            ) : (
+              <svg role="status" class="mr-2 w-8 h-8 text-gray-900 animate-spin fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+              </svg>
+            )} {unit ? unit : ""}</h3>
+          </div>
+          <div class="ml-auto self-center rounded-full bg-green-500 p-2">{icon}</div>
         </div>
-        <h3 className='text-2xl font-bold'>{localValue ? (
-          humanFunction(value + localAdd)
-        ) : (
-          <svg role="status" class="mr-2 w-8 h-8 text-gray-900 animate-spin fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-          </svg>
-        )} {unit ? unit : ""}</h3>
-        <p>{title}</p>
-        <div className=''>
-          <span className=' flex-row flex'>
-            <div className='flex flex-col align-middle'>
-              <ArrowUpIcon className="h-5 w-5" />
-            </div>
-            {humanFunction(rate)}/s</span>
+        <div class="flex flex-row items-end">
+          <div class="mt-2">
+            <span class="flex-row flex items-end">
+              <ArrowUpIcon className="h-5 w-5 text-green-400" />
 
+              {humanFunction(rate)}/s
+
+              {subtext ? <p className='text-sm text-gray-500 ml-2'>{subtext}</p> : ""}
+            </span>
+          </div>
         </div>
-        {subtext ? <p className='text-xs text-gray-500'>{subtext}</p> : ""}
       </div>
-      <style jsx>{`
-
-      .stats-icon {
-        text-align: center;
-        line-height: 50px;
-        margin-left: 15px;
-        color: #000;
-        position: absolute;
-        right: -5px;
-        top: 20px;
-        opacity: .1;
-        font-size: 6em;
-            }
-      `}</style>
     </>
   )
 
